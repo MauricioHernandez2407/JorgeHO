c25ddc4c0e71ae7c51509de886ea55ea98683045
Jhrra <72525182+Jhrra@users.noreply.github.com>
2020-10-07 16:18:02 -0500

    Práctica 1, Actividad 5

---
+++ HolaMundo2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+#include<stdlib.h>
+ int main()
+{
+	printf("Hola mundo\n");
+	getch();
+	printf("Jorge Herrera\n");
+	getch();
+	printf("Voy a compartir el codigo con mi compañero\n");
+	getch();
+	system("cls");
+	printf("Hola mundo\n");
+	getch();
+	return 0;	
+}
